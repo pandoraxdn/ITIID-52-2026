da9b11027408c4a5f30e1ad53447d2ff5f573207
pandoraxdn <pandoraxdn@protonmail.com>
2026-01-09 08:16:09 -0600

    09/01/2026

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+
+// Variables y Constantes
+//const nombre:string = "Juan Pablo";
+//let edad: number = 21;
+
+//console.log(nombre, typeof(nombre));
+//console.log(edad, typeof(edad));
+
+
+const App = () => {
+    return <div>Hola dsm 52</div>;
+}
+
+export default App;
